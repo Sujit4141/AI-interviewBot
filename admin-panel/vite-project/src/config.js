15e42f43322d857bf96eb74857b9965ba7110e9b
Jitsu <sujitku641@gmail.com>
2026-04-02 09:27:24 +0530

    fix: hardcode API URL

--- admin-panel/vite-project/src/config.js
+++ admin-panel/vite-project/src/config.js
@@ -1,3 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"||"https://ai-interview-bot-backend-hjms.onrender.com";
+// For LOCAL testing — uncomment this:
+// const API_BASE_URL = "http://localhost:5000";
+
+// For PRODUCTION — uncomment this:
+const API_BASE_URL = "https://ai-interview-bot-backend-hjms.onrender.com";
 
 export default API_BASE_URL;
